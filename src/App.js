c89294bf52c44fccdace26486820ceaae08ed2d5
Sujith-S07 <sujithsakthivel07@gmail.com>
2026-07-02 00:48:47 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   const[student,setstudent]=useState("");
+  const[msg,setmsg]=useState("");
+
    const[number,setnumber]=useState(0);
    
    useEffect(()=>{
@@ -16,11 +18,11 @@ function App() {
     return number*2;
    },[number])
    const Message=useCallback(()=>{
-   alert("completed successfully")});
+   setmsg("completed successfully")},[]);
    const Start=useCallback(()=>{
-   alert("On process")});
+   setmsg("On process")},[]);
    const Finish=useCallback(()=>{
-   console.log("I Am Finished My Task Very Well")});
+   setmsg("I Am Finished My Task Very Well")},[]);
    const handleSubmit=()=>{
     if(student.trim()===""){ 
       alert("please enter the student name");
@@ -35,20 +37,23 @@ function App() {
     <div className="App">
       <header className="STUDENTS DETAILS">
         <h1 >UseState Task</h1>
+        
         <h3>Student Name Display</h3>
         <input type="text" placeholder='studentName' value={student} onChange={(e)=>setstudent(e.target.value)}></input><br></br>
         <button onClick={(handleSubmit)}>Submit</button>
+         <hr></hr>
         <h1>useMemo Task</h1>
+       
         <h3>Multiple Number by 2</h3>
         <h3>Number:{number}</h3>
         <h3>Result:{result}</h3>
         <button onClick={()=>setnumber(number+1)}>output</button><br></br>
+        <hr></hr>
         <h1>show Welcome Message</h1>
           <button onClick={ Message}> Message1</button>
           <button onClick={ Start}> Message2</button>
           <button onClick={ Finish}> Message3</button>
-
-        
+        <p>{msg}</p>
       </header>
     </div>
   );
